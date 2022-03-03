08c1c3f68e25456f47c3e6177bd030ee85738f83
clog10 <carlosloaeza100598@gmail.com>
2022-03-03 12:21:05 -0600

    Commons RestDirecciones

--- RESTDirecciones/src/main/java/com/ibm/academia/restapi/direcciones/RestDireccionesApplication.java
+++ RESTDirecciones/src/main/java/com/ibm/academia/restapi/direcciones/RestDireccionesApplication.java
@@ -1,13 +1,18 @@
 package com.ibm.academia.restapi.direcciones;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
+@EntityScan({"com.ibm.academia.restapi.commons.modelo.entidades"})
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class RestDireccionesApplication {
 
 	public static void main(String[] args) {
